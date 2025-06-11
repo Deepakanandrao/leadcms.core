b89d7ff0df6843162dcd99ee68fd63f1eb49e9d0
Peter Liapin <pl@duklas.com>
2025-06-11 16:23:22 +0530

    Allow anonymous access to content types

--- src/LeadCMS/Controllers/ContentTypesController.cs
+++ src/LeadCMS/Controllers/ContentTypesController.cs
@@ -20,4 +20,26 @@ public class ContentTypesController : BaseControllerWithImport<ContentType, Cont
         : base(dbContext, mapper, esDbContext, queryProviderFactory)
     {
     }
+
+    [HttpGet]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public override Task<ActionResult<List<ContentTypeDetailsDto>>> Get([FromQuery] string? query)
+    {
+        return base.Get(query);
+    }
+
+    [HttpGet("{id}")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public override Task<ActionResult<ContentTypeDetailsDto>> GetOne(int id)
+    {
+        return base.GetOne(id);
+    }
 }
