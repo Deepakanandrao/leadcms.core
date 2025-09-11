2add91b2a0394d2400898c3f5084afe9c34ee40c
Peter Liapin <pl@duklas.com>
2025-09-11 09:02:25 +0530

    Make Language property nullable in Contact entity

--- src/LeadCMS/Entities/Contact.cs
+++ src/LeadCMS/Entities/Contact.cs
@@ -85,7 +85,7 @@ public class Contact : BaseEntity, ICommentable
     public int? Timezone { get; set; }
 
     [Searchable]
-    public string Language { get; set; } = string.Empty;
+    public string? Language { get; set; }
 
     [Searchable]
     public string? TranslationKey { get; set; }
