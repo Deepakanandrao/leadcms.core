87cfc509295425531ca38ce278ba225852025a6b
Peter Liapin <pl@duklas.com>
2026-04-23 15:54:09 +0530

    Update package references and enhance EmailService with message ID generation tests

--- src/LeadCMS/Services/EmailService.cs
+++ src/LeadCMS/Services/EmailService.cs
@@ -10,6 +10,7 @@ using MailKit;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
+using MimeKit.Utils;
 
 namespace LeadCMS.Services;
 
@@ -49,7 +50,7 @@ public class EmailService : IEmailService
 
             await client.SendAsync(message);
 
-            return message.MessageId;
+            return message.MessageId ?? throw new InvalidOperationException("Email message ID was not generated.");
         }
         catch (Exception exception)
         {
@@ -81,6 +82,8 @@ public class EmailService : IEmailService
     private static async Task<MimeMessage> GenerateEmailBody(string subject, string fromEmail, string fromName, string[] recipients, string body, List<AttachmentDto>? attachments)
     {
         var message = new MimeMessage();
+
+        message.MessageId = MimeUtils.GenerateMessageId();
 
         message.Subject = subject;
 
